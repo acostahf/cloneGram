9838611850b0aece29b3c66888966397bac16af3
Fabian Acosta <acosahf@hotmail.com>
2020-05-12 22:21:27 -0500

    add middleware for auth so that user must be signed in, the create function is tied to the user through auth

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,20 +6,31 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
+
+    public function _construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function create()
     {
-        return view('posts/create');
+        return view('posts.create');
     }
     public function store()
     {
        $data = request()->validate([
-           'another' => '',
            'caption' => 'required',
-           'image' => 'required|image',
+           'image' => ['required','image'],
        ]);
 
+       $imagePath = request('image')->store('uploads', 'public');
 
-       auth()->user()->posts()->create($data);
+       auth()->user()->posts()->create([
+           'caption' => $data['caption'],
+           'image' => $imagePath,
+       ]);
+
+       return redirect('/profile/'. auth()->user()->id);
 
     }
 }
