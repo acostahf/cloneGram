8b05903c874623b2d4c9ff8b4fda7328d9497b7b
Fabian Acosta <acosahf@hotmail.com>
2020-05-16 15:17:16 -0500

    follow using vue.js components

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -3,17 +3,23 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-3 pb-5">
-            <img src="{{ $user->profile->profileImage() }}" class="w-100" class="rounded-circle">
+        <div class="col-3 p-5">
+            <img src="{{ $user->profile->profileImage() }}" class="rounded-circle w-100">
         </div>
         <div class="col-9 pt-5">
             <!--passing data to the view-->
-            @can ('update', $user->profile)
             <div class="d-flex justify-content-between align-items-baseline ">
-                <h1>{{ $user->username}}</h1>
-                <a href="/p/create">Adds New Post</a>
-            </div>
+                <div class="d-flex align-items-center pb-3">
+                    <div class="h4">{{ $user->username}}</div>
 
+                    <follow-btn user-id="{{ $user->id }}"></follow-btn>
+                </div>
+
+                @can ('update', $user->profile)
+                <a href="/p/create">Adds New Post</a>
+                @endcan
+            </div>
+            @can ('update', $user->profile)
             <a href="/profile/{{$user->id}}/edit">Edit Profile</a>
             @endcan
 
