df893a199c5cfb5cc51200b1142b3cae15d0a3bb
Fabian Acosta <acosahf@hotmail.com>
2020-05-16 13:01:32 -0500

    automatically creating a profile using model events

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ProfilesController extends Controller
 {
@@ -24,7 +25,7 @@ class ProfilesController extends Controller
     public function update(User $user){
 
         $this->authorize('update', $user->profile); 
-        
+
         $data = request()->validate([
         'title' => 'required',
         'description' => 'required',
@@ -33,7 +34,20 @@ class ProfilesController extends Controller
         ]
     );
 
-    $user->profile->update($data);
+    
+    if (request('image')){
+        $imagePath = request('image')->store('profile', 'public');
+        
+        
+        $image = Image::make(public_path("storage/{$imagePath}"))->fit(1000, 1000);
+        $image->save();
+        
+    }
+
+    auth()->user()->profile->update(array_merge(
+        $data,
+        ['image' => $imagePath]
+    ));
 
     return redirect('/profile/'. auth()->user()->id);
     }
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,8 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-3 pb-5">
-            <img src="https://scontent-dfw5-2.cdninstagram.com/v/t51.2885-19/s150x150/38802638_1995233293874851_6860449586587107328_n.jpg?_nc_ht=scontent-dfw5-2.cdninstagram.com&_nc_ohc=PcSSGRr60OEAX-BNEXn&oh=73cf9e0193fb2dfa5f9b37273b495b0c&oe=5EE536C4"
-                class="rounded-circle">
+            <img src="/storage/{{ $user->profile->image}}" class="w-100" class="rounded-circle">
         </div>
         <div class="col-9 pt-5">
             <!--passing data to the view-->
